fb7f30c94f1d258c62a0d6597f0c167fef0043ce
xstefank <xstefank122@gmail.com>
2017-07-30 18:40:57 +0200

    mod_cluster update

---
+++ PolicyModule/src/main/java/org/xstefank/learn/Policy1.java
@@ -0,0 +1,9 @@
+package org.xstefank.learn;
+
+import java.security.Policy;
+
+/**
+ * @author <a href="mailto:xstefank122@gmail.com">Martin Stefanko</a>
+ */
+public class Policy1 extends Policy {
+}
