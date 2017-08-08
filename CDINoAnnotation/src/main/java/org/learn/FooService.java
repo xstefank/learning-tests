3cc5c97377d3dd41b31c6bd5ec9b9e0a2825444e
xstefank <xstefank122@gmail.com>
2017-08-08 08:53:15 +0200

    CDINoAnnotation add beans.xml

--- CDINoAnnotation/src/main/java/org/learn/FooService.java
+++ CDINoAnnotation/src/main/java/org/learn/FooService.java
@@ -1,11 +1,8 @@
 package org.learn;
 
-import javax.enterprise.context.ApplicationScoped;
-
 /**
  * @author <a href="mailto:xstefank122@gmail.com">Martin Stefanko</a>
  */
-@ApplicationScoped
 public class FooService {
 
     String sayFoo(String name) {
