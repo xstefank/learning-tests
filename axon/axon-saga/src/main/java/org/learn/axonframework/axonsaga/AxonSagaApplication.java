0c47ee43f137eed1c749b0f495f4fd0f18fae9c8
xstefank <xstefank122@gmail.com>
2017-09-03 10:01:07 +0200

    add axon-saga learning test

---
+++ axon/axon-saga/src/main/java/org/learn/axonframework/axonsaga/AxonSagaApplication.java
@@ -0,0 +1,12 @@
+package org.learn.axonframework.axonsaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonSagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxonSagaApplication.class, args);
+	}
+}
