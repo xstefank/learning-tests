7596a9bcdfb38ff02834f17b9af354b27f72d66c
xstefank <xstefank122@gmail.com>
2017-09-10 23:45:16 +0200

    axon-service - add service-model project

---
+++ axon/axon-service/service-model/src/main/java/org/learn/axonframework/coreapi/OrderAPI.kt
@@ -0,0 +1,12 @@
+package org.learn.axonframework.coreapi
+
+import org.axonframework.commandhandling.TargetAggregateIdentifier
+
+class FileOrderCommand(@TargetAggregateIdentifier val orderId: String, val productName: String, val comment: String, val price: Int)
+class PrepareShipmentCommand(val orderId: String, val productName: String, val price: Int)
+class CreateInvoiceCommand(val orderId: String, val productName: String, val comment: String)
+
+
+class OrderFiledEvent(val orderId: String, val productName: String, val comment: String, val price: Int)
+class ShipmentPreparedEvent(val shipmentId : String, val orderId: String)
+class InvoiceCreatedEvent(val invoiceId : String, val orderId: String)
