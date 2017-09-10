25535b2bcb80f64c49e1cd4c2530a8409a5c398c
xstefank <xstefank122@gmail.com>
2017-09-10 22:42:34 +0200

    axon-service - add shipment-service project

---
+++ axon/axon-service/shipment-service/src/main/java/org/learn/axonframework/shipmentservice/ShipmentServiceApplication.java
@@ -0,0 +1,12 @@
+package org.learn.axonframework.shipmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipmentServiceApplication.class, args);
+	}
+}
---
+++ axon/axon-service/shipment-service/src/test/java/org/learn/axonframework/shipmentservice/ShipmentServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package org.learn.axonframework.shipmentservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShipmentServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
