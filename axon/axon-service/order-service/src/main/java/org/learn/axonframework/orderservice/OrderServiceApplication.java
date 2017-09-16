ddd42c9a504a2193c352f66136adbb628e5aa981
xstefank <xstefank122@gmail.com>
2017-09-16 23:29:58 +0200

    axon-service - order-service connection to Eureka

--- axon/axon-service/order-service/src/main/java/org/learn/axonframework/orderservice/OrderServiceApplication.java
+++ axon/axon-service/order-service/src/main/java/org/learn/axonframework/orderservice/OrderServiceApplication.java
@@ -16,8 +16,10 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class OrderServiceApplication {
 
