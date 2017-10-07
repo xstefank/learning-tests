ec18839e694f3ce5afb743a770084e2368f72b21
xstefank <xstefank122@gmail.com>
2017-10-07 20:02:06 +0200

    add stuff project for Java 8

---
+++ stuff8/src/main/java/org/learn/stuff8/Main.java
@@ -0,0 +1,23 @@
+package org.learn.stuff8;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] stringArray = new String[10];
+
+        System.out.println(Arrays.toString(stringArray));
+
+        for (String string : stringArray) {
+            System.out.println(string);
+        }
+
+        System.out.println("----------------- object");
+        Object[] objectArray = new Object[10];
+
+        System.out.println(Arrays.toString(objectArray));
+        
+
+    }
+}
