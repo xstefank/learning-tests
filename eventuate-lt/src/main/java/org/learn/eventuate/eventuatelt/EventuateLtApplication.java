50a4877b189c876a48600d9c3c22b861c27944d1
xstefank <xstefank122@gmail.com>
2017-10-10 22:03:13 +0200

    eventuate-lt is now spring boot application

---
+++ eventuate-lt/src/main/java/org/learn/eventuate/eventuatelt/EventuateLtApplication.java
@@ -0,0 +1,12 @@
+package org.learn.eventuate.eventuatelt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventuateLtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventuateLtApplication.class, args);
+	}
+}
