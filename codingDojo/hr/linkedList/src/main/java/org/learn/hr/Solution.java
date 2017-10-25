b1c8f28402b2497801d155d21ec498c6da757297
xstefank <xstefank122@gmail.com>
2017-10-25 23:47:58 +0200

    hr linked list - insertFirst + insertNth

--- codingDojo/hr/linkedList/src/main/java/org/learn/hr/Solution.java
+++ codingDojo/hr/linkedList/src/main/java/org/learn/hr/Solution.java
@@ -32,7 +32,43 @@ public class Solution {
         tail.next.next = null;
         return head;
     }
-    
+
+    public Node insertFirst(Node head, int x) {
+        Node newHead = new Node();
+        newHead.data = x;
+        newHead.next = head;
+        return  newHead;
+
+    }
+
+    public Node insertNth(Node head, int data, int position) {
+        Node current = head;
+        Node newNode = new Node();
+        newNode.data = data;
+
+        if (current == null) {
+            return newNode;
+        }
+
+        if (position == 0) {
+            newNode.next = head;
+            return newNode;
+        }
+
+        int i = 0;
+        while (i < position - 1&& current.next != null) {
+            current = current.next;
+            i++;
+        }
+
+        Node nextElem = current.next;
+
+        current.next = newNode;
+        newNode.next = nextElem;
+
+        return head;
+    }
+
     private class Node {
 
         //testing purposes
