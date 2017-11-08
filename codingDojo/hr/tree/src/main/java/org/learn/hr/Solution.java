3a80c994d2961a49284a635adf1e2db955e3ec46
xstefank <xstefank122@gmail.com>
2017-11-08 09:18:14 +0100

    tree preOrder traversal

--- codingDojo/hr/tree/src/main/java/org/learn/hr/Solution.java
+++ codingDojo/hr/tree/src/main/java/org/learn/hr/Solution.java
@@ -2,7 +2,17 @@ package org.learn.hr;
 
 public class Solution {
 
-    
+    public static void preOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+
     private static class Node {
         int data;
         Node left;
