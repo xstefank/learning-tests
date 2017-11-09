238949821e306851ccfcbdfa0194454037724384
xstefank <xstefank122@gmail.com>
2017-11-09 10:31:35 +0100

    hr tree post order traversal

--- codingDojo/hr/tree/src/main/java/org/learn/hr/Solution.java
+++ codingDojo/hr/tree/src/main/java/org/learn/hr/Solution.java
@@ -2,16 +2,25 @@ package org.learn.hr;
 
 public class Solution {
 
-    public static void preOrder(Node root) {
-        if (root == null) {
+    public static void preOrder(Node node) {
+        if (node == null) {
             return;
         }
 
-        System.out.print(root.data + " ");
-        preOrder(root.left);
-        preOrder(root.right);
+        System.out.print(node.data + " ");
+        preOrder(node.left);
+        preOrder(node.right);
     }
 
+    public static void postOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.data+ " ");
+    }
 
     private static class Node {
         int data;
