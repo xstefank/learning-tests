6393a13f7b72de74cf070f71c7a9cc8a18523842
xstefank <xstefank122@gmail.com>
2017-11-09 22:50:04 +0100

    hr trees inOrder and height

--- codingDojo/hr/tree/src/main/java/org/learn/hr/Solution.java
+++ codingDojo/hr/tree/src/main/java/org/learn/hr/Solution.java
@@ -22,6 +22,27 @@ public class Solution {
         System.out.print(node.data+ " ");
     }
 
+    public static void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left);
+        System.out.print(node.data + " ");
+        inOrder(node.right);
+    }
+
+    static int height(Node node) {
+        if (node == null) {
+            return -1;
+        }
+
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
     private static class Node {
         int data;
         Node left;
