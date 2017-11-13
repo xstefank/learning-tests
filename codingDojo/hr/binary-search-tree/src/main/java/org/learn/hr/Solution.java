bbe0c5c6d0fd53119bfa8ceef0b6b25312d60a26
xstefank <xstefank122@gmail.com>
2017-11-13 09:11:07 +0100

    hr BST insert

--- codingDojo/hr/binary-search-tree/src/main/java/org/learn/hr/Solution.java
+++ codingDojo/hr/binary-search-tree/src/main/java/org/learn/hr/Solution.java
@@ -2,7 +2,33 @@ package org.learn.hr;
 
 public class Solution {
 
-    -
+    static Node insert(Node node,int value) {
+        if (node == null) {
+            return newNode(value);
+        }
+
+        if (value < node.data) {
+            if (node.left != null) {
+                insert(node.left, value);
+            } else {
+                node.left = newNode(value);
+            }
+        } else {
+            if (node.right != null) {
+                insert(node.right, value);
+            } else {
+                node.right = newNode(value);
+            }
+        }
+
+        return node;
+    }
+
+    private static Node newNode(int data) {
+        Node node = new Node();
+        node.data = data;
+        return node;
+    }
 
 
     private static class Node {
