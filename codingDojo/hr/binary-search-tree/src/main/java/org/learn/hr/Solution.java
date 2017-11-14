16c6c21e1292765d6a9ce6cea468247cb966ff79
xstefank <xstefank122@gmail.com>
2017-11-14 20:09:26 +0100

    hr last common ancestor in binary search tree

--- codingDojo/hr/binary-search-tree/src/main/java/org/learn/hr/Solution.java
+++ codingDojo/hr/binary-search-tree/src/main/java/org/learn/hr/Solution.java
@@ -30,6 +30,39 @@ public class Solution {
         return node;
     }
 
+    static Node lca(Node node, int v1, int v2) {
+        if (node.data < v1 && node.data < v2) {
+            return lca(node.right, v1, v2);
+        }
+
+        if (node.data > v1 && node.data > v2) {
+            return lca(node.left, v1, v2);
+        }
+
+        return node;
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node();
+        root.data = 8;
+        root.left = new Node();
+        root.left.data = 4;
+        root.left.right = new Node();
+        root.left.right.data = 6;
+        root.left.right.left = new Node();
+        root.left.right.left.data = 5;
+        root.left.left = new Node();
+        root.left.left.data = 1;
+        root.left.left.right = new Node();
+        root.left.left.right.data = 2;
+        root.left.left.right.right = new Node();
+        root.left.left.right.right.data = 3;
+        root.right = new Node();
+        root.right.data = 9;
+
+        System.out.println(lca(root, 1, 2).data);
+    }
+
 
     private static class Node {
 
