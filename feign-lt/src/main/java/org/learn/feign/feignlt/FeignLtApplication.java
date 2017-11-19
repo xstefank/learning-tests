848e7ab79951d292c8be926a642f35978acabcaa
xstefank <xstefank122@gmail.com>
2017-11-19 23:00:02 +0100

    add feign lt

---
+++ feign-lt/src/main/java/org/learn/feign/feignlt/FeignLtApplication.java
@@ -0,0 +1,12 @@
+package org.learn.feign.feignlt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignLtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignLtApplication.class, args);
+	}
+}
