d0f3eaac5490a2f5b86bdf86fa964ca8eb8b627d
xstefank <xstefank122@gmail.com>
2017-11-26 14:20:38 +0100

    stuff8 testing regex

--- stuff8/src/main/java/org/learn/stuff8/Main.java
+++ stuff8/src/main/java/org/learn/stuff8/Main.java
@@ -1,23 +1,35 @@
 package org.learn.stuff8;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
+//    public static void main(String[] args) {
+//        String[] stringArray = new String[10];
+//
+//        System.out.println(Arrays.toString(stringArray));
+//
+//        for (String string : stringArray) {
+//            System.out.println(string);
+//        }
+//
+//        System.out.println("----------------- object");
+//        Object[] objectArray = new Object[10];
+//
+//        System.out.println(Arrays.toString(objectArray));
+//
+//
+//    }
+
     public static void main(String[] args) {
-        String[] stringArray = new String[10];
+        String test = "foo1:8080/api";
+        Pattern pattern = Pattern.compile("^(.*/api).*", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(test);
 
-        System.out.println(Arrays.toString(stringArray));
-
-        for (String string : stringArray) {
-            System.out.println(string);
+        if (matcher.find()) {
+            System.out.println(matcher.group(1));
         }
-
-        System.out.println("----------------- object");
-        Object[] objectArray = new Object[10];
-
-        System.out.println(Arrays.toString(objectArray));
-
-
     }
 }
