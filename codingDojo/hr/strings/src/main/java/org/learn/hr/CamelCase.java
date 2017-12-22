4b3f299dff2d07245d1945a25f722425b2f8f877
xstefank <xstefank122@gmail.com>
2017-12-22 09:18:02 +0100

    hr add strings module; complete CamelCase

---
+++ codingDojo/hr/strings/src/main/java/org/learn/hr/CamelCase.java
@@ -0,0 +1,19 @@
+package org.learn.hr;
+
+import java.util.Scanner;
+
+public class CamelCase {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(ClassLoader.getSystemResourceAsStream("input.txt"));
+        String s = in.next();
+
+
+        int upperCaseCount = countUpperCaseLetters(s);
+        System.out.println(upperCaseCount == 0 ? 1 : upperCaseCount + 1);
+    }
+
+    private static int countUpperCaseLetters(String s) {
+        return (int) s.chars().filter(Character::isUpperCase).count();
+    }
+}
