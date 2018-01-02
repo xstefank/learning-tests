acda10a8e13139b237fc2c717a16732f808a2b9c
xstefank <xstefank122@gmail.com>
2018-01-02 11:25:25 +0100

    div while program fix

--- while-program/src/main/java/org/learn/DivWP.java
+++ while-program/src/main/java/org/learn/DivWP.java
@@ -5,17 +5,15 @@ public class DivWP {
     public int apply(int x, int y) {
         int z = 0;
         int u = 0;
-        while (u < x) {
+        int c = y;
+        while (c <= x) {
             int v = 0;
             while (v != y) {
                 v = v + 1;
                 u = u + 1;
             }
             z = z + 1;
-        }
-
-        if (u > x) {
-            z = z - 1;
+            c = c + y;
         }
 
         return z;
