a19d08847e057775e538a983e8c698d51444bec5
xstefank <xstefank122@gmail.com>
2018-02-03 22:34:47 +0100

    bash - add arbitrary_vars_cycle.sh

---
+++ bash/arbitrary_vars_cycle.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Cycles through the arbitrary number of the variables
+# in format of 'num_*' from 1 while the vars are set
+
+
+i=1
+current=num_$i
+
+while [ ! -z ${!current} ]; do
+  echo ${!current}
+  i=$((i+1))
+  current=num_$i
+done
+
+
+
