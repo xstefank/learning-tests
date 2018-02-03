0b0d457ca5c72cee69f27742daca92b84bfb08af
xstefank <xstefank122@gmail.com>
2018-02-03 11:28:00 +0100

    jacson generics learning test

--- jackson-generic-lt/src/main/java/org/xstefank/lt/App.java
+++ jackson-generic-lt/src/main/java/org/xstefank/lt/App.java
@@ -1,13 +1,29 @@
 package org.xstefank.lt;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class App {
+
+    public static void main(String[] args) throws IOException {
+//        saveJSON();
+        readJSON();
     }
+
+    private static void saveJSON() throws IOException {
+        Wrapper wrapper = new Wrapper<StringBuffer>("test wrapper", new StringBuffer("test data"));
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(new File("test.json"), wrapper);
+    }
+
+    private static void readJSON() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        Wrapper wrapper = mapper.readValue(new File("test.json"), Wrapper.class);
+
+        System.out.println(wrapper.getData().getClass());
+    }
+
 }
---
+++ jackson-generic-lt/src/main/java/org/xstefank/lt/Wrapper.java
@@ -0,0 +1,40 @@
+package org.xstefank.lt;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use=JsonTypeInfo.Id.CLASS, include=JsonTypeInfo.As.PROPERTY, property="@class")
+public class Wrapper<T> {
+
+    private String name;
+    private T data;
+
+    public Wrapper() {
+
+    }
+
+    public Wrapper(String name, T data) {
+        this.name = name;
+        this.data = data;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Wrapper: %s | %s", getName(), getData());
+    }
+}
