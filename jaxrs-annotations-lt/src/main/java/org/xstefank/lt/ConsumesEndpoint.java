0d8d36bc3244b2284ab95e0c704501b727f94987
xstefank <xstefank122@gmail.com>
2018-02-08 08:05:04 +0100

    Add consumes endpoint and move away from web.xml in jaxrs-annotations-lt

---
+++ jaxrs-annotations-lt/src/main/java/org/xstefank/lt/ConsumesEndpoint.java
@@ -0,0 +1,18 @@
+package org.xstefank.lt;
+
+import org.jboss.logging.Logger;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/consumes")
+@Produces(MediaType.APPLICATION_JSON)
+public class ConsumesEndpoint {
+
+    @POST
+    public void postJSON(DummyJson json) {
+        Logger.getLogger(ConsumesEndpoint.class).info("received " + json.getName());
+    }
+}
---
+++ jaxrs-annotations-lt/src/main/java/org/xstefank/lt/DummyJson.java
@@ -0,0 +1,14 @@
+package org.xstefank.lt;
+
+public class DummyJson {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
