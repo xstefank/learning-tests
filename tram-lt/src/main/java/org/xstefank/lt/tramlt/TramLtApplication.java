0417bdb5dcece323bf4561ccdbf2702449576f26
xstefank <xstefank122@gmail.com>
2018-02-20 19:21:42 +0100

    Add Eventuate Tram lt

---
+++ tram-lt/src/main/java/org/xstefank/lt/tramlt/TramLtApplication.java
@@ -0,0 +1,12 @@
+package org.xstefank.lt.tramlt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TramLtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TramLtApplication.class, args);
+	}
+}
