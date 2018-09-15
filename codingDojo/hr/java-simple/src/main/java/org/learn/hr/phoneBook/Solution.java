7649ea8e194ed6874cf8b19648a164e57b4ccdfb
xstefank <xstefank122@gmail.com>
2018-09-15 19:23:16 +0200

    Add hr java phone book

---
+++ codingDojo/hr/java-simple/src/main/java/org/learn/hr/phoneBook/Solution.java
@@ -0,0 +1,11 @@
+package org.learn.hr.phoneBook;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    }
+}
