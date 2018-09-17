1f02feb95a327f416dca2ce9f6cd91dcb37589e8
xstefank <xstefank122@gmail.com>
2018-09-17 22:43:33 +0200

    Add hr java phone book

--- codingDojo/hr/java-simple/src/main/java/org/learn/hr/phoneBook/Solution.java
+++ codingDojo/hr/java-simple/src/main/java/org/learn/hr/phoneBook/Solution.java
@@ -7,5 +7,22 @@ import java.util.Scanner;
 public class Solution {
 
     public static void main(String[] args) {
+        Map<String, Integer> phoneNumbers = new HashMap<>();
+        
+        Scanner in = new Scanner(ClassLoader.getSystemResourceAsStream("phoneBook.txt"));
+        int n = in.nextInt();
+        in.nextLine();
+        for (int i = 0; i < n; i++) {
+            String name = in.nextLine();
+            int phone = in.nextInt();
+            phoneNumbers.put(name, phone);
+            in.nextLine();
+        }
+        while (in.hasNext()) {
+            String s = in.nextLine();
+            Integer phoneNumber = phoneNumbers.get(s);
+            System.out.println(phoneNumber != null ? s + "=" + phoneNumber : "Not found");
+            
+        }
     }
 }
