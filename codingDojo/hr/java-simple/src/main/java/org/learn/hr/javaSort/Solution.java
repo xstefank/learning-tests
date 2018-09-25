eaec4564e4c12296409127989753ac8e7e4b0087
xstefank <xstefank122@gmail.com>
2018-09-25 23:42:32 +0200

    Add hr java sort

---
+++ codingDojo/hr/java-simple/src/main/java/org/learn/hr/javaSort/Solution.java
@@ -0,0 +1,57 @@
+package org.learn.hr.javaSort;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(ClassLoader.getSystemResourceAsStream("javaSort.txt"));
+        int testCases = Integer.parseInt(in.nextLine());
+
+        List<Student> studentList = new ArrayList<>();
+        while (testCases > 0) {
+            int id = in.nextInt();
+            String fname = in.next();
+            double cgpa = in.nextDouble();
+
+            Student st = new Student(id, fname, cgpa);
+            studentList.add(st);
+
+            testCases--;
+        }
+        
+        studentList.sort(Comparator.comparing(Student::getCgpa).reversed().thenComparing(Student::getFname).thenComparing(Student::getId));
+
+        for (Student st : studentList) {
+            System.out.println(st.getFname());
+        }
+    }
+}
+
+class Student {
+    private int id;
+    private String fname;
+    private double cgpa;
+
+    public Student(int id, String fname, double cgpa) {
+        super();
+        this.id = id;
+        this.fname = fname;
+        this.cgpa = cgpa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public double getCgpa() {
+        return cgpa;
+    }
+}
