c4110c6010c42c1b3fcc4c9493d4920eeb3bda5a
xstefank <xstefank122@gmail.com>
2018-09-30 17:59:38 +0200

    Add hr java inheritance 1

---
+++ codingDojo/hr/java-simple/src/main/java/org/learn/hr/javaInheritance1/Solution.java
@@ -0,0 +1,30 @@
+package org.learn.hr.javaInheritance1;
+
+class Animal {
+    void walk() {
+        System.out.println("I am walking");
+    }
+}
+
+
+class Bird extends Animal {
+    void fly() {
+        System.out.println("I am flying");
+    }
+
+    void sing() {
+        System.out.println("I am singing");
+    }
+}
+
+public class Solution {
+
+    public static void main(String args[]) {
+
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.sing();
+
+    }
+}
