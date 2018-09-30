f21206c3fddc668870c9ee586c244e50b1cca841
xstefank <xstefank122@gmail.com>
2018-09-30 18:09:38 +0200

    Add hr java instanceof keyword

---
+++ codingDojo/hr/java-simple/src/main/java/org/learn/hr/javaInstancofKeyword/InstanceOFTutorial.java
@@ -0,0 +1,40 @@
+package org.learn.hr.javaInstancofKeyword;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Student{}
+class Rockstar{   }
+class Hacker{}
+
+
+public class InstanceOFTutorial {
+
+    static String count(ArrayList mylist){
+        int a = 0,b = 0,c = 0;
+        for(int i = 0; i < mylist.size(); i++){
+            Object element=mylist.get(i);
+            if(element instanceof Student)
+            a++;
+            if(element instanceof Rockstar)
+            b++;
+            if(element instanceof Hacker)
+            c++;
+        }
+        String ret = Integer.toString(a)+" "+ Integer.toString(b)+" "+ Integer.toString(c);
+        return ret;
+    }
+
+    public static void main(String []args){
+        ArrayList mylist = new ArrayList();
+        Scanner sc = new Scanner(ClassLoader.getSystemResourceAsStream("javaInstanceofKeyword.txt"));
+        int t = sc.nextInt();
+        for(int i=0; i<t; i++){
+            String s=sc.next();
+            if(s.equals("Student"))mylist.add(new Student());
+            if(s.equals("Rockstar"))mylist.add(new Rockstar());
+            if(s.equals("Hacker"))mylist.add(new Hacker());
+        }
+        System.out.println(count(mylist));
+    }
+}
