c0be405a241388361243036b52dd2de67349ef34
xstefank <xstefank122@gmail.com>
2018-10-01 23:38:27 +0200

    Add hr java reflection

---
+++ codingDojo/hr/java-simple/src/main/java/org/learn/hr/javaReflection/Solution.java
@@ -0,0 +1,41 @@
+package org.learn.hr.javaReflection;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Student{
+    private String name;
+    private String id;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void anothermethod(){  }
+    
+}
+
+public class Solution {
+
+    public static void main(String[] args){
+        Class student = Student.class;
+        Method[] methods = student.getDeclaredMethods();
+
+        ArrayList<String> methodList = new ArrayList<>();
+        for(Method method : methods){
+            methodList.add(method.getName());
+        }
+        Collections.sort(methodList);
+        for(String name: methodList){
+            System.out.println(name);
+        }
+    }
+
+}
