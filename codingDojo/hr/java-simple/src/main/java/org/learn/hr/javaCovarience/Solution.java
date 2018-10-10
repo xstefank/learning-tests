4dc5d5b6d503b1aa811fa1fa15b475c163607e9a
xstefank <xstefank122@gmail.com>
2018-10-10 22:00:24 +0200

    Add hr java covarience

---
+++ codingDojo/hr/java-simple/src/main/java/org/learn/hr/javaCovarience/Solution.java
@@ -0,0 +1,64 @@
+package org.learn.hr.javaCovarience;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//Complete the classes below
+class Flower {
+    String whatsYourName() {
+        return "I have many names and types.";
+    }
+}
+
+class Jasmine extends Flower {
+    @Override
+    String whatsYourName() {
+        return "Jasmine";
+    }
+}
+
+class Lily extends Flower{
+    @Override
+    String whatsYourName() {
+        return "Lily";
+    }
+}
+
+class Region {
+    Flower yourNationalFlower() {
+        return new Flower();
+    }
+}
+
+class WestBengal extends Region {
+    @Override
+    Jasmine yourNationalFlower() {
+        return new Jasmine();
+    }
+}
+
+class AndhraPradesh extends Region {
+    @Override
+    Lily yourNationalFlower() {
+        return new Lily();
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(ClassLoader.getSystemResourceAsStream("javaCovarience.txt")));
+        String s = reader.readLine().trim();
+        Region region = null;
+        switch (s) {
+            case "WestBengal":
+                region = new WestBengal();
+                break;
+            case "AndhraPradesh":
+                region = new AndhraPradesh();
+                break;
+        }
+        Flower flower = region.yourNationalFlower();
+        System.out.println(flower.whatsYourName());
+    }
+}
