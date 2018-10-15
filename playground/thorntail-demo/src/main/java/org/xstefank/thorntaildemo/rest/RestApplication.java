de604ac26228c1e8848cf2c4c9a1bdbd53afb328
xstefank <xstefank122@gmail.com>
2018-10-15 21:49:49 +0200

    Add playground and thorntail demo

---
+++ playground/thorntail-demo/src/main/java/org/xstefank/thorntaildemo/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package org.xstefank.thorntaildemo.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+
+}
