20632eed4a7ea1d23ddb51af47c7c39145a4921c
xstefank <xstefank122@gmail.com>
2018-11-09 23:16:08 +0100

    Add Microprofile Health lt

---
+++ microprofile/health-lt/src/main/java/org/xstefnak/healthlt/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package org.xstefnak.healthlt.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+
+}
