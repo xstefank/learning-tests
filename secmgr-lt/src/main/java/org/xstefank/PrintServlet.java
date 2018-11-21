fb7220ea0ec6c778496c3f03c56590b447ff4ef7
xstefank <xstefank122@gmail.com>
2018-11-21 15:10:16 +0100

    Clean up secmgr-lt

--- secmgr-lt/src/main/java/org/xstefank/PrintServlet.java
+++ secmgr-lt/src/main/java/org/xstefank/PrintServlet.java
@@ -6,18 +6,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet("/*")
 public class PrintServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        PrintWriter writer = new PrintWriter("/home/mstefank/workspace/file.txt", "UTF-8");
-//        writer.println("The first line");
-//        writer.println("The second line");
-//        writer.close();
-
         resp.getWriter().println(System.getProperty("java.home"));
         
         resp.getWriter().write("Successfully written file");
