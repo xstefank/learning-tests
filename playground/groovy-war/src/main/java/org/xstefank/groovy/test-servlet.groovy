2f7bd1282703ca88f48cb1acf5b835dcf9c61775
xstefank <xstefank122@gmail.com>
2018-11-25 19:21:44 +0100

    Update groovy-war

--- playground/groovy-war/src/main/java/org/xstefank/groovy/test-servlet.groovy
+++ playground/groovy-war/src/main/java/org/xstefank/groovy/test-servlet.groovy
@@ -1,16 +1,12 @@
 package org.xstefank.groovy
 
-import javax.servlet.ServletException
-import javax.servlet.annotation.WebServlet
-import javax.servlet.http.HttpServlet
-import javax.servlet.http.HttpServletRequest
-import javax.servlet.http.HttpServletResponse
+def name = params.name ?: "World"
 
-@WebServlet("/*")
-class TestServlet extends HttpServlet{
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("Hello from Groovy script")
+html.html {
+    head {
+        title "Hello from Groovlet!"
+    }
+    body {
+        h1 "Hello, ${name}!"
     }
 }
