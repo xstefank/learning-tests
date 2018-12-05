02cd14cd8511b36fb70b8253eec4d602acd50db6
xstefank <xstefank122@gmail.com>
2018-12-05 10:04:24 +0100

    Add maven-env lt

---
+++ maven-env/src/test/java/org/xstefank/AppTest.java
@@ -0,0 +1,13 @@
+package org.xstefank;
+
+import org.junit.Test;
+
+public class AppTest {
+    
+    @Test
+    public void test() {
+        System.out.println(System.getenv());
+        System.out.println("----------------");
+        System.out.println(System.getProperties());
+    }
+}
