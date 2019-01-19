d720f5f37ca0c125641541ad260bc7a58d634a9e
xstefank <xstefank122@gmail.com>
2019-01-20 00:03:04 +0100

    Add JSON-P lt

---
+++ json-p/src/main/java/org/xstefank/PingResource.java
@@ -0,0 +1,27 @@
+package org.xstefank;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import java.io.StringReader;
+
+@Path("ping")
+public class PingResource {
+    
+    @GET
+    public Response ping() {
+        JsonObject json = Json.createObjectBuilder()
+                .add("name", "Iron Man")
+                .add("realName", "Tony Start")
+                .add("alive", "true")
+                .build();
+
+        JsonReader reader = Json.createReader(new StringReader(json.toString()));
+        System.out.println(reader.readObject().getString("name"));
+
+        return Response.ok(json.toString()).build();
+    }
+}
