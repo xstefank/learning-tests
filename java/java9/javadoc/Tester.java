f73952c0b3aa30ac3375a0e12e3d67dcadd152c4
xstefank <xstefank122@gmail.com>
2019-02-02 15:38:25 +0100

    Add java9 lt - javadoc

---
+++ java/java9/javadoc/Tester.java
@@ -0,0 +1,15 @@
+/**
+  * @author MahKumar
+  * @version 0.1
+*/
+public class Tester {
+   /**
+      * Default method to be run to print 
+      * <p>Hello world</p>
+      * @param args command line arguments
+   */
+   public static void main(String []args) {
+      System.out.println("Hello World");
+   }
+}
+
---
+++ java/java9/javadoc/out/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Tester","l":"main(String[])","url":"main-java.lang.String:A-"},{"p":"<Unnamed>","c":"Tester","l":"Tester()"}]
