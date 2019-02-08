9ddd4c332b0e0f79ffa0360529d6a07fe33ddfcc
xstefank <xstefank122@gmail.com>
2019-02-08 21:34:48 +0100

    Add cdi-bservers2 tweaking

--- cdi-observes2/src/main/java/org/xstefank/TestClassClient.java
+++ cdi-observes2/src/main/java/org/xstefank/TestClassClient.java
@@ -1,5 +1,18 @@
 package org.xstefank;
 
-@RegisterLRAParticipant
+import org.eclipse.microprofile.lra.annotation.Compensate;
+import org.eclipse.microprofile.lra.annotation.Complete;
+
+@RegisterLRAParticipant(name = "TestParticipant")
 public class TestClassClient {
+    
+    @Compensate
+    public void compensate() {
+        
+    }
+    
+    @Complete
+    public void complete() {
+        
+    }
 }
