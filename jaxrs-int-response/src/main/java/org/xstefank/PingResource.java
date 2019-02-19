56abebea3f77f92d354a103bbcd4095032ef203e
xstefank <xstefank122@gmail.com>
2019-02-19 09:30:07 +0100

    Add jaxrs-int-response lt

---
+++ jaxrs-int-response/src/main/java/org/xstefank/PingResource.java
@@ -0,0 +1,31 @@
+package org.xstefank;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+@Path("ping")
+public class PingResource {
+
+    @Context
+    private UriInfo uriInfo;
+    
+    @GET
+    public Response ping() {
+        Response response = ClientBuilder.newClient().target(uriInfo.getBaseUriBuilder().path("ping/client").build())
+                .request().get();
+
+        int i = response.readEntity(Integer.class);
+
+        return Response.ok("Application deployed successfully " + i).build();
+    }
+    
+    @GET
+    @Path("client")
+    public Response getClient() {
+        return Response.ok(42).build();
+    }
+}
