421c2b9cfe18ac2a4ecc0486771d9523fab9f270
xstefank <xstefank122@gmail.com>
2019-02-22 09:04:03 +0100

    Add java testing folder

---
+++ playground/java/URLTest.java
@@ -0,0 +1,9 @@
+import java.net.URL;
+
+import java.net.MalformedURLException;
+
+public class URLTest {
+    public static void main(String[] args) throws MalformedURLException {
+        new URL("http://localhost:8080/lra-coordinator/0_ffff0a2805c7_10029f27_5c6faa6c_24");
+    }
+}
