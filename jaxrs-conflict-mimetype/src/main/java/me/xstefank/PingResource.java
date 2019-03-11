287c2a9b22196b3947acdf2877a51c4a9558ade3
xstefank <xstefank122@gmail.com>
2019-03-11 08:38:56 +0100

    Add jaxrs-conflict-mimetype lt

---
+++ jaxrs-conflict-mimetype/src/main/java/me/xstefank/PingResource.java
@@ -0,0 +1,18 @@
+package me.xstefank;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Produces(MediaType.APPLICATION_XML)
+@Path("/ping")
+public class PingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String hello() {
+        return "hello";
+    }
+}
