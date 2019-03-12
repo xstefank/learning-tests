70f744c8324a0f12936e0e8bfaf5c2dcee397783
xstefank <xstefank122@gmail.com>
2019-03-12 15:39:50 +0100

    Addd thread-local-wfly lt

---
+++ thread-local-wfly/src/main/java/org/xstefank/PingResource.java
@@ -0,0 +1,27 @@
+package org.xstefank;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/ping")
+public class PingResource {
+
+    private ThreadLocal<String> threadLocal = new ThreadLocal<>();
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "hello " + threadLocal.get();
+    }
+
+    @POST
+    @Path("{value}")
+    public String provide(@PathParam("value") String value) {
+        threadLocal.set(value);
+        return "Value set";
+    }
+}
