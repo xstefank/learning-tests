0c96a4a984ba21cab6cbb77affccce12550c5bc4
xstefank <xstefank122@gmail.com>
2019-03-13 10:37:56 +0100

    Add quarkus config-new-prop lt

---
+++ quarkus/config-new-prop/src/test/java/me/xstefank/NativePingResourceIT.java
@@ -0,0 +1,9 @@
+package me.xstefank;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativePingResourceIT extends PingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
