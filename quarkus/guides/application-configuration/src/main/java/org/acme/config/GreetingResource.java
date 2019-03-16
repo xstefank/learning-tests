8db4ac3d72a82da14434ce187297734301b7600e
xstefank <xstefank122@gmail.com>
2019-03-16 20:53:27 +0100

    Add quarkus config guide

---
+++ quarkus/guides/application-configuration/src/main/java/org/acme/config/GreetingResource.java
@@ -0,0 +1,32 @@
+package org.acme.config;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.Optional;
+
+@Path("/greeting")
+public class GreetingResource {
+
+    @Inject
+    @ConfigProperty(name = "greeting.message")
+    private String message;
+
+    @Inject
+    @ConfigProperty(name = "greeting.suffix", defaultValue="!")
+    private String suffix;
+
+    @Inject
+    @ConfigProperty(name = "greeting.name")
+    private Optional<String> name;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return message + " " + name.orElse("world") + suffix;
+    }
+}
