fd0be288f2a17c29676485e40389766d4c59ff78
xstefank <xstefank122@gmail.com>
2019-03-16 21:05:17 +0100

    Add thorntail provider config injection lt

---
+++ thorntail/config-provider/src/main/java/org/xstefank/PingResource.java
@@ -0,0 +1,21 @@
+package org.xstefank;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("ping")
+public class PingResource {
+
+    @Inject
+    @ConfigProperty(name="myprj.some.dynamic.timeout", defaultValue="100")
+    private javax.inject.Provider<Long> timeout;
+
+    @GET
+    public Response ping() {
+        return Response.ok("Application running successfully " + timeout.get()).build();
+    }
+}
