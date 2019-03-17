e7adbbec3c6ee5aee7f1ec8ba3906dd4f184015f
xstefank <xstefank122@gmail.com>
2019-03-17 09:01:13 +0100

    Add list injection to thorntail config dynamic provider property lt

--- thorntail/config/src/main/java/io/xstefank/config/rest/HelloWorldEndpoint.java
+++ thorntail/config/src/main/java/io/xstefank/config/rest/HelloWorldEndpoint.java
@@ -8,6 +8,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
+import java.util.List;
 
 @ApplicationScoped
 @Path("/hello")
@@ -17,9 +18,11 @@ public class HelloWorldEndpoint {
 	@ConfigProperty(name="myprj.some.dynamic.timeout", defaultValue="100")
 	private javax.inject.Provider<Long> timeout;
 
+	@Inject @ConfigProperty(name="myPets") private List<String> myListPets;
+
 	@GET
 	@Produces("text/plain")
 	public Response doGet() {
-		return Response.ok("Hello from Thorntail! " + timeout.get()).build();
+		return Response.ok("Hello from Thorntail! " + myListPets).build();
 	}
 }
