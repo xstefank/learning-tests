1a74ab95a1506a6d220f429d66255e8421b2fdce
xstefank <xstefank122@gmail.com>
2019-03-17 07:44:52 +0100

    Add thorntail config dynamic provider property lt

---
+++ thorntail/config/src/main/java/io/xstefank/config/rest/HelloWorldEndpoint.java
@@ -0,0 +1,25 @@
+package io.xstefank.config.rest;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+
+@ApplicationScoped
+@Path("/hello")
+public class HelloWorldEndpoint {
+
+	@Inject
+	@ConfigProperty(name="myprj.some.dynamic.timeout", defaultValue="100")
+	private javax.inject.Provider<Long> timeout;
+
+	@GET
+	@Produces("text/plain")
+	public Response doGet() {
+		return Response.ok("Hello from Thorntail! " + timeout.get()).build();
+	}
+}
