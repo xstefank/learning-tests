b159b0f026dabb501a36d4a20b015a2b5694b689
xstefank <xstefank122@gmail.com>
2019-03-17 12:38:47 +0100

    Add quarkus app lifecycle guide

---
+++ quarkus/guides/application-lifecycle-events/src/main/java/org/acme/events/AppLifecycleBean.java
@@ -0,0 +1,24 @@
+package org.acme.events;
+
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+
+@ApplicationScoped
+public class AppLifecycleBean {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger("ListenerBean");
+
+    void onStart(@Observes StartupEvent ev) {
+        LOGGER.info("The application is starting...");
+    }
+
+    void onStop(@Observes ShutdownEvent ev) {
+        LOGGER.info("The application is stopping...");
+    }
+
+}
