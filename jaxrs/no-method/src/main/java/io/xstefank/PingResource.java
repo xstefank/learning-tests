5f72da2590cfcfa58e6ef9e03a56a26a7eceefcc
xstefank <xstefank122@gmail.com>
2019-03-22 14:55:05 +0100

    Add no-method jaxrs lt

---
+++ jaxrs/no-method/src/main/java/io/xstefank/PingResource.java
@@ -0,0 +1,15 @@
+package io.xstefank;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/ping")
+public class PingResource {
+    
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "hello";
+    }
+}
