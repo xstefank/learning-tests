342000718fb79ebc96b618063c6aae791cd8b28c
xstefank <xstefank122@gmail.com>
2019-03-30 13:54:19 +0100

    add quarkus transactions guide

---
+++ quarkus/guides/transactions/src/test/java/io/xstefank/NativePingResourceIT.java
@@ -0,0 +1,9 @@
+package io.xstefank;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativePingResourceIT extends PingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
