d8cd5474ca88e3f208d5ba7be2cfe9fadcffbc3e
xstefank <xstefank122@gmail.com>
2019-04-27 18:01:03 +0200

    Add LRA lt for returning wrong LRA statuses

---
+++ lra-statuses/client/src/main/java/io/xstefank/LRAParticipant.java
@@ -0,0 +1,49 @@
+package io.xstefank;
+
+import org.eclipse.microprofile.lra.annotation.Compensate;
+import org.eclipse.microprofile.lra.annotation.Complete;
+import org.eclipse.microprofile.lra.annotation.ParticipantStatus;
+import org.eclipse.microprofile.lra.annotation.Status;
+import org.eclipse.microprofile.lra.annotation.ws.rs.LRA;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("lra")
+public class LRAParticipant {
+
+    @GET
+    @Path("doInLRA")
+    @LRA(LRA.Type.REQUIRED)
+    public Response doInLRA(@HeaderParam(LRA.LRA_HTTP_CONTEXT_HEADER) String lraId) {
+        System.out.println("enlisting and ending LRA " + lraId);
+        return Response.ok().build();
+    } 
+    
+    @PUT
+    @Path("complete")
+    @Complete
+    public Response complete(@HeaderParam(LRA.LRA_HTTP_CONTEXT_HEADER) String lraId) {
+        System.out.println("Completing " + lraId);
+        return Response.accepted().build();
+    }
+
+    @GET
+    @Path("status")
+    @Status
+    public Response status(@HeaderParam(LRA.LRA_HTTP_CONTEXT_HEADER) String lraId) {
+        System.out.println("Status for " + lraId);
+        return Response.ok(ParticipantStatus.Compensating).build();
+    } 
+    
+    @PUT
+    @Path("compensate")
+    @Compensate
+    public Response compensate(@HeaderParam(LRA.LRA_HTTP_CONTEXT_HEADER) String lraId) {
+        System.out.println("Compensating " + lraId);
+        return Response.ok().build();
+    }
+}
