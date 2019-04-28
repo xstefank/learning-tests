cf140e60db275e43e70499b5bdcc81b0e1c84d6f
xstefank <xstefank122@gmail.com>
2019-04-28 07:53:18 +0200

    Add thorntail jsonb with JsonObject lt

---
+++ thorntail/jsonb/src/main/java/io/xstefank/PingResource.java
@@ -0,0 +1,21 @@
+package io.xstefank;
+
+import javax.json.JsonObject;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("ping")
+public class PingResource {
+    
+    @GET
+    public Response ping() {
+        return Response.ok("Application running successfully").build();
+    }
+
+    @POST
+    public String post(JsonObject jsonObject) {
+        return jsonObject.toString();
+    }
+}
