a2262f835ad7c5c2745c41915099e2822cbaa910
xstefank <xstefank122@gmail.com>
2019-05-29 22:53:32 +0200

    Add ssl-native quarkus guide

---
+++ quarkus/guides/ssl-native/rest-client/src/test/java/org/acme/restclient/CountriesResourceIT.java
@@ -0,0 +1,10 @@
+package org.acme.restclient;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class CountriesResourceIT extends CountriesResourceTest {
+
+    // Run the same tests
+
+}
