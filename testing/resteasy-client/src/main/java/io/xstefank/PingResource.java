34303b272561f89c2579e901c6173806e99f9082
xstefank <xstefank122@gmail.com>
2019-05-31 12:41:19 +0200

    Add more testing to rest-client

--- testing/resteasy-client/src/main/java/io/xstefank/PingResource.java
+++ testing/resteasy-client/src/main/java/io/xstefank/PingResource.java
@@ -1,17 +1,26 @@
 package io.xstefank;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/ping")
 public class PingResource {
     
+    @Inject
+    @ConfigProperty(name = "ints")
+    private List<Integer> ints;
+    
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        GitHubAPI.update();
+        System.out.println(ints);
+//        GitHubAPI.update();
         
         return "hello";
     }
