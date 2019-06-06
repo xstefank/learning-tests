fedbf7377f1201a348bce36821713e146b81d901
xstefank <xstefank122@gmail.com>
2019-06-06 11:39:17 +0200

    Add ReaderInterceptor test to jaxrs-quarkus lt

--- jaxrs-quarkus/src/main/java/io/xstefank/PingResource.java
+++ jaxrs-quarkus/src/main/java/io/xstefank/PingResource.java
@@ -1,10 +1,12 @@
 package io.xstefank;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
@@ -52,5 +54,23 @@ public class PingResource {
     @Path("async-client")
     public CompletionStage<String> asyncClient() {
         return CompletableFuture.supplyAsync(() -> "original string");
+    }
+
+    @GET
+    @Path("interceptor")
+    public String interceptor() {
+        Client client = ClientBuilder.newClient();
+//        client.register();
+
+        Response response = client.target(uriInfo.getBaseUriBuilder().path("interceptor-client").build())
+            .request().post(Entity.text("test"));
+
+        return response.readEntity(String.class);
+    }
+    
+    @POST
+    @Path("interceptor-client")
+    public String interceptor(String s) {
+        return s;
     }
 }
