f12a1769d2b696b14f42a6a00195cb3e908dc1e9
xstefank <xstefank122@gmail.com>
2019-06-12 10:01:34 +0200

    Update jandex-lt with multiple streams indexing example

--- jandex-lt/src/main/java/io/xstefank/PingResource.java
+++ jandex-lt/src/main/java/io/xstefank/PingResource.java
@@ -1,15 +1,19 @@
 package io.xstefank;
 
 import org.jboss.jandex.AnnotationInstance;
+import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.DotName;
 import org.jboss.jandex.Index;
 import org.jboss.jandex.IndexReader;
+import org.jboss.jandex.Indexer;
+import org.jboss.jandex.MethodInfo;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @Path("/ping")
@@ -30,6 +34,32 @@ public class PingResource {
             System.out.println("XXXXXXX");
         }
 
+
+        return "hello";
+    }
+    
+    @GET
+    @Path("2")
+    public String jandexManually() throws IOException {
+        Indexer indexer = new Indexer();
+
+        InputStream stream = getClass().getClassLoader()
+            .getResourceAsStream("java/util/Map.class");
+        
+        indexer.index(stream);
+        
+        stream = getClass().getClassLoader()
+            .getResourceAsStream("java/util/Set.class");
+
+        indexer.index(stream);
+        
+        Index index = indexer.complete();
+
+        ClassInfo clazz = index.getClassByName(DotName.createSimple("java.util.Map"));
+        System.out.println(clazz);
+
+        clazz = index.getClassByName(DotName.createSimple("java.util.Set"));
+        System.out.println(clazz);
 
         return "hello";
     }
