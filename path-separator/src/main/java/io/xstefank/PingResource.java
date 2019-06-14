f067b25147ff96b053b8523c76e3569b3b73890e
xstefank <xstefank122@gmail.com>
2019-06-14 09:54:49 +0200

    Add path-separator lt

---
+++ path-separator/src/main/java/io/xstefank/PingResource.java
@@ -0,0 +1,20 @@
+package io.xstefank;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.io.File;
+
+@Path("/ping")
+public class PingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        System.out.println("Path separator: " + System.getProperty("path.separator"));
+        System.out.println("Path separator: " + File.pathSeparator);
+        
+        return "hello";
+    }
+}
