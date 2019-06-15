780725dfe8c5e60b670b3b6dac158b1a1babd016
xstefank <xstefank122@gmail.com>
2019-06-15 17:46:35 +0200

    Clear redundant unfunctional code from lra-statuses

--- lra-statuses/client/src/main/java/io/xstefank/PingResource.java
+++ lra-statuses/client/src/main/java/io/xstefank/PingResource.java
@@ -1,13 +1,8 @@
 package io.xstefank;
 
-import org.eclipse.microprofile.lra.annotation.Compensate;
-import org.eclipse.microprofile.lra.annotation.ParticipantStatus;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
-import java.net.URI;
 
 @Path("ping")
 public class PingResource {
@@ -17,13 +12,4 @@ public class PingResource {
         return Response.ok("Application running successfully").build();
     }
     
-    @Compensate
-    public void cancelBooking(URI lraId) {
-        if (//something) {
-            throw new RuntimeException(); // results in FailedToCompensate
-        } else if (// something else) {
-            throw new WebApplicationException(Response.accepted().build()); // indicate Compensating
-        }
-        // else cancel the booking with id lraId
-    }
 }
