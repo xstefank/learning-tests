f3f70db99a99a7192da954785701d09743d8ff20
xstefank <xstefank122@gmail.com>
2020-03-15 19:35:26 +0100

    Add hr birthday-candles

--- codingDojo/hr-quarkus/src/main/java/io/xstefank/HRResource.java
+++ codingDojo/hr-quarkus/src/main/java/io/xstefank/HRResource.java
@@ -319,4 +319,45 @@ public class HRResource {
         return sum;
     }
 
+    @GET
+    @Path("birthday-candles")
+    public void birthdayCandles() throws Exception {
+        Scanner scanner = new Scanner(hrService.getFile("birthday-candles.txt"));
+
+        int arCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arCount; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = birthdayCakeCandles(ar);
+
+        System.out.println(result);
+
+        scanner.close();
+    }
+
+    private static int birthdayCakeCandles(int[] ar) {
+        int count = 0;
+        int currentMax = 0;
+
+        for (int i : ar) {
+            if (i > currentMax) {
+                currentMax = i;
+                count = 1;
+            } else if (i == currentMax) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
 }
