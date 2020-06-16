18b7f79392542f0980dc60846d5797e8ad4c3788
xstefank <xstefank122@gmail.com>
2020-06-16 18:02:18 +0200

    Add even more testing for mutiny-lt

--- mutiny-lt/src/main/java/io/xstefank/PingResource.java
+++ mutiny-lt/src/main/java/io/xstefank/PingResource.java
@@ -7,6 +7,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Path("/ping")
 public class PingResource {
@@ -14,20 +15,25 @@ public class PingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        AtomicInteger counter = new AtomicInteger(0);
+        
         Uni<Integer> uni1 = Uni.createFrom().item(1);
-        Uni<Integer> uni2 = Uni.createFrom().failure(() -> new Exception());
-        Uni<Integer> uni3 = Uni.createFrom().item(3);
+        Uni<Integer> uni3 = Uni.createFrom().item(() -> counter.getAndIncrement());
 
-        List<Uni<Integer>> unis = List.of(uni1, uni2, uni3);
+        List<Uni<Integer>> unis = List.of(uni1, uni3);
 
         Uni<Integer> integerUni = Uni.combine().all().unis(unis)
             .combinedWith(results ->
             {
-                results.forEach(System.out::println);
+                System.out.println(results);
+                System.out.println("=====================");
                 return 0;
             }).onFailure().recoverWithItem(-1);
 
         System.out.println(integerUni.await().indefinitely());
+        System.out.println(integerUni.await().indefinitely());
+        System.out.println(integerUni.await().indefinitely());
+        
         return "hello";
     }
 }
