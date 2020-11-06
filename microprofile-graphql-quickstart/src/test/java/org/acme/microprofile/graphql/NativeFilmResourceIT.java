0d6eef88f4a7ee81ed426094e513be53071503a7
xstefank <xstefank122@gmail.com>
2020-11-06 11:08:55 +0100

    Add mp-graphql lt

---
+++ microprofile-graphql-quickstart/src/test/java/org/acme/microprofile/graphql/NativeFilmResourceIT.java
@@ -0,0 +1,9 @@
+package org.acme.microprofile.graphql;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeFilmResourceIT extends FilmResourceTest {
+
+    // Execute the same tests but in native mode.
+}
