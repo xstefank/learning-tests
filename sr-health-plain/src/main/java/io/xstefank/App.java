915502fabc4a5c7e6560db4ca4d728f46491c36c
xstefank <xstefank122@gmail.com>
2022-01-20 14:21:57 +0100

    Add more testing to sr-health-plain

--- sr-health-plain/src/main/java/io/xstefank/App.java
+++ sr-health-plain/src/main/java/io/xstefank/App.java
@@ -6,13 +6,21 @@ import io.smallrye.health.api.HealthType;
 import io.smallrye.health.registry.HealthRegistries;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 
+import java.util.Collection;
+
 public class App {
     public static void main(String[] args) {
         HealthRegistry livenessRegistry = HealthRegistries.getRegistry(HealthType.LIVENESS);
+        HealthRegistry readinessRegistry = HealthRegistries.getRegistry(HealthType.READINESS);
+        HealthRegistry startupRegistry = HealthRegistries.getRegistry(HealthType.STARTUP);
+        HealthRegistry wellnessRegistry = HealthRegistries.getRegistry(HealthType.WELLNESS);
+
+        HealthRegistry testGroupRegistry = HealthRegistries.getHealthGroupRegistry("test-group");
+        Collection<HealthRegistry> healthGroupRegistries = HealthRegistries.getHealthGroupRegistries();
 
         livenessRegistry.register("test-hc", () -> HealthCheckResponse.up("test"));
 
-        SmallRyeHealthReporter smallRyeHealthReporter = new SmallRyeHealthReporter();
-        smallRyeHealthReporter.reportHealth(System.out, smallRyeHealthReporter.getHealth());
+        SmallRyeHealthReporter reporter = new SmallRyeHealthReporter();
+        reporter.reportHealth(System.out, reporter.getHealth());
     }
 }
