41e4bd4752b811123c4e8807a378d43aea6c5a61
xstefank <xstefank122@gmail.com>
2022-04-07 17:58:11 +0200

    Update rest-client-annotation for reactive versions

--- rest-client-annotation/src/main/java/io/xstefank/MyFilter.java
+++ rest-client-annotation/src/main/java/io/xstefank/MyFilter.java
@@ -1,19 +1,22 @@
 package io.xstefank;
 
-import org.jboss.resteasy.client.jaxrs.internal.ClientRequestContextImpl;
+import org.jboss.resteasy.reactive.client.impl.ClientRequestContextImpl;
 
 import javax.ws.rs.client.ClientRequestContext;
 import javax.ws.rs.client.ClientRequestFilter;
 import javax.ws.rs.ext.Provider;
+import java.lang.reflect.Method;
 
 @Provider
 public class MyFilter implements ClientRequestFilter {
     @Override
     public void filter(ClientRequestContext requestContext) {
-        Class<?> declaringClass = ((ClientRequestContextImpl) requestContext)
-            .getInvocation()
-            .getClientInvoker()
-            .getDeclaring();
+        ClientRequestContextImpl clientRequestContext = (ClientRequestContextImpl) requestContext;
+        Method method = (Method) clientRequestContext
+            .getRestClientRequestContext()
+            .getProperties()
+            .get("org.eclipse.microprofile.rest.client.invokedMethod");
+        Class<?> declaringClass = method.getDeclaringClass();
 
         MyAnnotation myAnnotation = declaringClass.getAnnotation(MyAnnotation.class);
         System.out.println(myAnnotation.value());
