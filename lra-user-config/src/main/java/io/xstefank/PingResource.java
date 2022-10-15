ec144e896896be6829df6568125736e33cb2e271
xstefank <xstefank122@gmail.com>
2022-10-15 16:41:18 +0200

    Add lra-user-config

--- lra-user-config/src/main/java/io/xstefank/PingResource.java
+++ lra-user-config/src/main/java/io/xstefank/PingResource.java
@@ -1,24 +1,46 @@
 package io.xstefank;
 
-import javax.ws.rs.HeaderParam;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.HeaderParam;
+import jakarta.ws.rs.PUT;
+import org.eclipse.microprofile.lra.annotation.Compensate;
+import org.eclipse.microprofile.lra.annotation.Complete;
 import org.eclipse.microprofile.lra.annotation.ws.rs.LRA;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.core.Response;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+import java.net.URI;
 
 @Path("ping")
+@ApplicationScoped
 public class PingResource {
     
     @GET
-    @LRA
+    @LRA(value = LRA.Type.REQUIRED)
     public Response ping(@HeaderParam(LRA.LRA_HTTP_CONTEXT_HEADER) String lraId) {
-        System.out.println("lraId = " + lraId);
+        System.out.println("ACTION lraId = " + lraId);
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         return Response.ok("Application deployed successfully").build();
+    }
+
+    @PUT
+    @Path("/compensate")
+    @Compensate
+    public Response compensate(@HeaderParam(LRA.LRA_HTTP_CONTEXT_HEADER) String lraId) {
+        System.out.println("COMPENSATE lraId = " + lraId);
+        return Response.ok().build();
+    }
+
+    @PUT
+    @Path("/complete")
+    @Complete
+    public void complete(@HeaderParam(LRA.LRA_HTTP_CONTEXT_HEADER) URI lraId) {
+        System.out.println("COMPLETE lraId = " + lraId.toASCIIString());
     }
 }
