87785e2d388158bda89148d3cea9716e934a4a9e
xstefank <xstefank122@gmail.com>
2022-11-30 09:28:07 +0100

    Add lra-wfly

---
+++ lra-wfly/src/main/java/io/xstefank/RestApplication.java
@@ -0,0 +1,9 @@
+package io.xstefank;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("")
+public class RestApplication extends Application {
+    
+}
