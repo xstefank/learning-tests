51069fb54a0abc05205a032eb050555b8f334471
xstefank <xstefank122@gmail.com>
2024-11-22 13:18:29 +0100

    Nullable finish

--- kotlin/NullReferences/src/io/xstefank/nullreferences/NullRerferences.kt
+++ kotlin/NullReferences/src/io/xstefank/nullreferences/NullRerferences.kt
@@ -1,13 +1,24 @@
 package io.xstefank.nullreferences
 
 fun main() {
-    val str: String? = null
-    println("String: ${str?.uppercase()}")
+//    val str: String? = null
+//    str?.let { println(it) }
+//
+//    val str4: String? = null
+//    val anotherStr = "This isn't a nullable"
+//    println(str4 == anotherStr)
+//
+//    val str2 = str!!
+//    val str3 = str2.uppercase()
 
-    val str2 = str ?: "This is default value"
-    println("String2: ${str2.uppercase()}")
+    val nullableInts = arrayOfNulls<Int>(5)
+    for (i in nullableInts) {
+        println(i)
+    }
 
-    val something: Any = arrayOf(1, 2, 3, 4)
-    val str3 = something as? String
-    println("String3: ${str3?.uppercase()}")
+    println(nullableInts[3].toString())
+}
+
+fun printText(text: String) {
+    println(text)
 }
