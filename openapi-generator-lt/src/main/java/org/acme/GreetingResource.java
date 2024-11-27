442ce18d41daa517c7ce95ac7062effcb4d0ddd4
xstefank <xstefank122@gmail.com>
2024-11-27 13:04:47 +0100

    Add calls to openapi-generator

--- openapi-generator-lt/src/main/java/org/acme/GreetingResource.java
+++ openapi-generator-lt/src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,24 @@
 package org.acme;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.openapi.quarkus.openapi_yaml.api.ExtensionsApi;
 
 @Path("/hello")
 public class GreetingResource {
 
+    @Inject
+    @RestClient
+    ExtensionsApi extensionsApi;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        System.out.println(extensionsApi.extensions(null, null));
         return "Hello from Quarkus REST";
     }
 }
